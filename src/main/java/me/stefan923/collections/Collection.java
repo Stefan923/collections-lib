3e52a3e6697836f88f788961fa9ecdd8df6cfc64
Ovidiu Ștefan Popescu <stefanpopescu923@gmail.com>
2022-07-23 20:05:51 +0300

    Create Collection.java
    Added Collection interface

---
+++ src/main/java/me/stefan923/collections/Collection.java
@@ -0,0 +1,16 @@
+package me.stefan923.collections;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public interface Collection<E> {
+
+    E add(E element);
+    boolean remove(E element);
+    Optional<E> findFirst(Predicate<E> predicate);
+    Optional<E> findLast(Predicate<E> predicate);
+    E[] findAll(Predicate<E> predicate);
+    boolean exists(Predicate<E> predicate);
+    Object[] toArray();
+
+}
