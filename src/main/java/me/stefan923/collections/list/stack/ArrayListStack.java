6c0511653a9a4df72ded54aecf705f79077cf470
Ovidiu Ștefan Popescu <stefanpopescu923@gmail.com>
2022-08-01 20:38:28 +0300

    [#26] Added JUnit tests for ArrayListStack<E>

--- src/main/java/me/stefan923/collections/list/stack/ArrayListStack.java
+++ src/main/java/me/stefan923/collections/list/stack/ArrayListStack.java
@@ -4,6 +4,14 @@ import me.stefan923.collections.list.ArrayList;
 
 public class ArrayListStack<E> extends ArrayList<E> implements Stack<E> {
 
+    public ArrayListStack() {
+        super();
+    }
+
+    public ArrayListStack(E[] elements) {
+        super(elements);
+    }
+
     @Override
     public boolean push(E element) {
         return add(element);
