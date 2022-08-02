53871fde7ea3544e621117d62873f75a255c953a
Ovidiu Ștefan Popescu <stefanpopescu923@gmail.com>
2022-08-02 19:31:29 +0300

    [#28] Implemented push() method for ArrayListQueue<E>

---
+++ src/main/java/me/stefan923/collections/list/queue/ArrayListQueue.java
@@ -0,0 +1,30 @@
+package me.stefan923.collections.list.queue;
+
+import me.stefan923.collections.list.ArrayList;
+
+public class ArrayListQueue<E> extends ArrayList<E> implements Queue<E> {
+
+    public ArrayListQueue() {
+        super();
+    }
+
+    public ArrayListQueue(E[] elements) {
+        super(elements);
+    }
+
+    @Override
+    public boolean push(E element) {
+        return add(element);
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+}
