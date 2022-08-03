78fad60afebafb8c8bf4a18d929f84f37486a82b
Ovidiu Ștefan Popescu <stefanpopescu923@gmail.com>
2022-08-04 00:00:31 +0300

    [#15] Updated get() method signature to return Optional<V>

--- src/main/java/me/stefan923/collections/map/Map.java
+++ src/main/java/me/stefan923/collections/map/Map.java
@@ -1,10 +1,12 @@
 package me.stefan923.collections.map;
 
+import java.util.Optional;
+
 public interface Map<K, V> {
 
     boolean put(K key, V value);
     boolean remove(K key);
     boolean has(K key);
-    V get(K key);
+    Optional<V> get(K key);
 
 }
